fd45a43a15b0f0aebc3eb1d260d5a6e799db8b42
JA <Jakea350@protonmail.com>
2020-10-12 17:44:26 -0500

    Create bye_wsus.ps1 :hammer:

---
+++ bye_wsus.ps1
@@ -0,0 +1,19 @@
+# Quick Script to remove all WSUS client settings
+# Be warned: this ps1 also kills any BITS job
+
+Stop-Service -Name BITS
+Stop-Service -Name wuauserv
+
+Remove-Item "$env:ALLUSERSPROFILE\Microsoft\Network\Downloader\qmgr*.dat" -ErrorAction SilentlyContinue
+Remove-Item 'HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate' -Recurse SilentlyContinue
+
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v AccountDomainSid /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v PingID /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" /v SusClientId /f
+
+netsh winsock reset
+netsh winhttp reset proxy
+Get-BitsTransfer | Remove-BitsTransfer
+
+Start-Service -Name BITS
+Start-Service -Name wuauserv
