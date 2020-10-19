1eeacb99303acbfbf28319a6e027a67c4f84c021
JA <Jakea350@protonmail.com>
2020-10-18 20:54:28 -0500

    fixed and simplified

--- auto_update_to_WSLv2.ps1
+++ auto_update_to_WSLv2.ps1
@@ -1,14 +1,11 @@
-# quick script for updating existing WSL instances to WSL Version 2
-
-$nixes = wsl -l #list of the system's currently installed WSL distributions
-$msi = New-Object System.Diagnostics.ProcessStartInfo("$PSScriptRoot\wsl_update_x64.msi","")
+### A quick script for updating existing WSL instances to WSL Version 2
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart # Enable the Windows Subsystem for Linux
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart # Enable Virtual Machine feature for WSL2
 
-Start-BitsTransfer -Source https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -Destination $PSScriptRoot\wsl_update_x64.msi # Download Linux kernel update package
-
-$msi.Start(); # Install Linux Kernel update package
-$msi.WaitForExit();
+Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -Outfile "$PSScriptRoot\wsl_update_x64.msi" # Download the Linux kernel update package
+Start-Process "$PSScriptRoot\wsl_update_x64.msi" -Wait # Install the Linux kernel update package
 
 wsl --set-default-version 2 # Set WSL Version to as default version for all newly created distros
+wsl --set-version Debian 2 #Set WSL Version of current distro(s) to ver 2 - INCLUDE ANY OTHER INSTALLED WSL DISTROS YOU WANT TO UPGRADE HERE
+wsl --list --verbose # Print distros and versions to verify success
