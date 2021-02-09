e8a3e10288ce857329035d201ea5a475bf77bab7
JA <Jakea350@protonmail.com>
2021-02-09 11:10:01 -0600

    added comment

--- check_no_expiry_pws.ps1
+++ check_no_expiry_pws.ps1
@@ -1,3 +1,5 @@
+## Connect-AzureAD with creds
+
 Get-AzureADUser -All $true | Select-Object UserprincipalName,@{
     N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}
  }
