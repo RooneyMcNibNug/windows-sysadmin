8b7c8b3d56f4ec693ef0e09c8054896f369233ef
JA <Jakea350@protonmail.com>
2021-02-15 10:20:55 -0600

    nice template for ps1 scripts from pluralsight

---
+++ Base_PS_Script_Template.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+
+#Script Name
+#Creator
+#Date
+#Updated
+#References, if any  
+
+#Variables
+
+#Parameters
+
+#Enter Tasks Below as Remarks
+
