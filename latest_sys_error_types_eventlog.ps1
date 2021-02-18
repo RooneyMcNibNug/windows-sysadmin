768ffca3e9098112f44aa6be2cbbc931bd33d537
JA <Jakea350@protonmail.com>
2021-02-17 21:16:02 -0600

    Add files via upload

---
+++ latest_sys_error_types_eventlog.ps1
@@ -0,0 +1,23 @@
+## A quick script get the latest block of error sources from the System section of Windows Event Log
+
+Param(
+    [string]$Log = "System",
+    [string]$Computername = $env:COMPUTERNAME,
+    [int32]$Latest = 400,
+    [string]$ReportTitle = "Last 400 Event Log Report",
+    [Parameter(Mandatory,HelpMessage = "Enter the path for the HTML file.")]
+    [string]$Path
+)
+
+$data = Get-Eventlog -logname $Log -EntryType Error -Newest $Latest -ComputerName $Computername |
+    Group-object -Property Source -NoElement 
+
+# HTML report creation
+$footer = "<h5><i>This report was ran on $(Get-Date)</i></h5>"
+$css = "http://jdhitsolutions.com/sample.css"
+$precontent = "<H1>$Computername</H1><H2>Last $Latest error sources from $Log</H2>"
+
+$data |Sort-Object -Property Count, Name -Descending |
+    Select-Object Count, Name | 
+    ConvertTo-Html -Title $ReportTitle -PreContent $precontent  -PostContent $footer -CssUri $css |
+    Out-File -FilePath $Path
