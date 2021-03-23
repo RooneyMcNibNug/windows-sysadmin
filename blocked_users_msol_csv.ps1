8cd0310202d389b9f37f59738ca38a710f14e819
JA <Jakea350@protonmail.com>
2021-03-23 16:07:37 -0500

    added LastLogonDate option

--- blocked_users_msol_csv.ps1
+++ blocked_users_msol_csv.ps1
@@ -1,3 +1,3 @@
 ## quickly get users with "Blocked Sign-In" status after "> Connect-MsolService -Credential $msolCred" and export to a .CSV in main user dir.
 
-Get-MsolUser -All | Where {$_.BlockCredential -eq $True} | Select DisplayName,UserPrincipalName, IsLicensed | Export-CSV -Path .\blocked_o365_users.csv
+Get-MsolUser -All | Where {$_.BlockCredential -eq $True} | Select DisplayName,UserPrincipalName,LastLogonDate,IsLicensed | Export-CSV -Path .\blocked_o365_users.csv
