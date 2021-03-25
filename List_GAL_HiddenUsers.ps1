687e8a690adb58a5ef4a682bf51399d869fb7620
JA <Jakea350@protonmail.com>
2021-03-25 10:29:22 -0500

    "Address" not "Access"!

--- List_GAL_HiddenUsers.ps1
+++ List_GAL_HiddenUsers.ps1
@@ -1,3 +1,3 @@
-## Super quick one-liner to grab all users currently hidden from the Global Access List from Connect-ExchangeOnline and generate a .CSV with timestamp
+## Super quick one-liner to grab all users currently hidden from the Global Address List from Connect-ExchangeOnline and generate a .CSV with timestamp
 
 Get-Mailbox -ResultSize Unlimited | Where {$_.HiddenFromAddressListsEnabled -eq $True} | Select Name,PrimarySmtpAddress,ExchangeObjectId,RecipientTypeDetails,WhenCreated,WhenChanged,HiddenFromAddressListsEnabled | Export-CSV -Path .\UsersHiddenFromGAL_$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')).csv
