8dc331dd17e346bc13095220b747933b6273e40c
JA <Jakea350@protonmail.com>
2021-04-19 10:46:10 -0500

    Delete last_reboots.ps1

--- last_reboots.ps1
+++
@@ -1,4 +0,0 @@
-# Quick one-liner for checking last observed reboots from Windows Events Log
-# Displaying Host, User, and Timestamp
-
-Get-EventLog -log System -newest 1000 | Where-Object {$_.EventID -eq '1074'} | FT machinename, username, timegenerated -autosize
